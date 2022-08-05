c01ab9c02419d498d82a907e05a368f9096f54ba
Ben Osborne <osborne.ben@gmail.com>
2022-08-05 13:02:09 -0500

    multiple recipient fields working

--- src/NetSendGridEmailClient.Web/wwwroot/js/site.js
+++ src/NetSendGridEmailClient.Web/wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+function addRecipientField(recipientType) {
+    var newIndex = document
+        .querySelectorAll("input[data-recipientType=\"".concat(recipientType, "\"]"))
+        .length;
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'input-group mb-3');
+    var newInput = document.createElement('input');
+    newInput.setAttribute('type', 'email');
+    newInput.setAttribute('class', 'form-control');
+    newInput.setAttribute('data-recipientType', recipientType);
+    newInput.setAttribute('id', recipientType + "[".concat(newIndex, "]"));
+    newInput.setAttribute('name', recipientType);
+    newDiv.appendChild(newInput);
+    document
+        .querySelector("div[data-recipientType=\"".concat(recipientType, "\"]"))
+        .appendChild(newDiv);
+}
+function removeRecipientField(recipientType) {
+    var fields = document.querySelectorAll("input[data-recipientType=\"".concat(recipientType, "\"]"));
+    // don't let all fields be removed
+    if (fields.length === 1)
+        return;
+    document
+        .querySelector("div[data-recipientType=\"".concat(recipientType, "\"]"))
+        .removeChild(fields[fields.length - 1].parentNode);
+}
+//# sourceMappingURL=site.js.map
---
+++ src/NetSendGridEmailClient.Web/wwwroot/js/site.ts
@@ -0,0 +1,36 @@
+﻿
+function addRecipientField(recipientType: string) {
+
+    let newIndex = document
+        .querySelectorAll(`input[data-recipientType="${recipientType}"]`)
+        .length;
+
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'input-group mb-3');
+
+    let newInput = document.createElement('input');
+    newInput.setAttribute('type', 'email');
+    newInput.setAttribute('class', 'form-control');
+    newInput.setAttribute('data-recipientType', recipientType);
+    newInput.setAttribute('id', recipientType + `[${newIndex}]`);
+    newInput.setAttribute('name', recipientType);
+
+    newDiv.appendChild(newInput);
+
+    document
+        .querySelector(`div[data-recipientType="${recipientType}"]`)
+        .appendChild(newDiv);
+}
+
+function removeRecipientField(recipientType: string) {
+
+    let fields = document.querySelectorAll(`input[data-recipientType="${recipientType}"]`);
+
+    // don't let all fields be removed
+    if (fields.length === 1) return;
+
+    document
+        .querySelector(`div[data-recipientType="${recipientType}"]`)
+        .removeChild(fields[fields.length - 1].parentNode);
+}
+
